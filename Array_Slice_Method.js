e0687aa16906e9babc358c7bd0e102d8c8b63629
Karuppasamych <106671462+Karuppasamych@users.noreply.github.com>
2022-07-02 22:40:39 +0530

    Create Array_Slice_Method.js

---
+++ Array_Slice_Method.js
@@ -0,0 +1,5 @@
+const fruits = ['Banana', 'orange','Lemon','Kiwi','Apple','Mango']
+
+fruits.slice(2 , 5) // it will slice the index from 2 to 4 and return the new array with sliced elements left the last mentioned element
+
+console.log(fruits)
