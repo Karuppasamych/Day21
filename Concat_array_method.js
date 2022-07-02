728f4c9aeb5ecf05cd7ff90bda7cf4dbab911b1d
Karuppasamych <106671462+Karuppasamych@users.noreply.github.com>
2022-07-02 21:19:11 +0530

    Create Concat_array_method.js

---
+++ Concat_array_method.js
@@ -0,0 +1,3 @@
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+var addingArr = myGirls.concat(myBoys) // adding myGirls array elements into the myBoys array like this // Cecilie,Lone,Emil,Tobias,Linus
