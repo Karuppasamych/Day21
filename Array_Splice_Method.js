1239789ae585191a239f2b2a9ddbd07638434629
Karuppasamych <106671462+Karuppasamych@users.noreply.github.com>
2022-07-02 22:28:45 +0530

    Create Array_Splice_Method.js

---
+++ Array_Splice_Method.js
@@ -0,0 +1,5 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.splice(2, 0, "Lemon", "Kiwi"); // adding the element at position of index from likw this 2 , 0 is how many element is need to present
+
+console.log(fruits)
